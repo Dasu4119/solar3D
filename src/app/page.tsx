1fa4caeb15dfd1d7e0dcb13603f5ba824a53c471
Dasu4119 <dasanjaneyulu5@gmail.com>
2026-08-13 01:48:10 +0530

    feat: add Solar3D home page

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Solar3D</h1>
+      <p>Solar design platform foundation is ready.</p>
+    </main>
+  );
+}
